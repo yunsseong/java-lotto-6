3e65385c8c9533e6c5819507cc5813a0df18fc74
yunsseong <aster20@hanyang.ac.kr>
2024-07-18 00:11:00 +0900

    feat(Application): lottoController construct implement

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.Controller.LottoController;
+import lotto.Model.LottoStore;
+import lotto.Util.Util;
+import lotto.View.Message;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		LottoController lottoController = new LottoController(new LottoStore(), new Message(), new Util());
+		lottoController.run();
+	}
 }
