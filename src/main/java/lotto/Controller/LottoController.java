54f08ed9bb9badc60423db29cbca97ad688c8388
yunsseong <aster20@hanyang.ac.kr>
2024-07-18 00:14:34 +0900

    feat(LottoController, Message): lotto purchase and validate value

---
+++ src/main/java/lotto/Controller/LottoController.java
@@ -0,0 +1,43 @@
+package lotto.Controller;
+
+import lotto.Model.LottoStore;
+import lotto.Util.Util;
+import lotto.View.Message;
+
+public class LottoController {
+	private final LottoStore lottoStore;
+	private final Message message;
+
+	private final Util util;
+
+	public LottoController(LottoStore lottoStore, Message message, Util util) {
+		this.lottoStore = lottoStore;
+		this.message = message;
+		this.util = util;
+	}
+
+	public void run() {
+		purchase();
+	}
+
+	public int purchase() {
+		String amountString = "";
+		do {
+			amountString = message.scanner("구매금액을 입력해 주세요.");
+		} while (!validateAmount(amountString));
+		message.println(amountString);
+		return Integer.parseInt(amountString);
+	}
+
+	public boolean validateAmount(String amountString) {
+		try {
+			util.validateNotEmpty(amountString);
+			util.validateNumber(amountString);
+			lottoStore.validateLottoAmount(amountString);
+			return true;
+		} catch (Exception e) {
+			message.printError(e.getMessage());
+			return false;
+		}
+	}
+}
---
+++ src/main/java/lotto/View/Message.java
@@ -0,0 +1,26 @@
+package lotto.View;
+
+import java.util.regex.Pattern;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Util.Util;
+
+public class Message {
+
+	public void println(String message) {
+		System.out.println(message);
+	}
+
+	public void printError(String message) {
+		this.println("[ERROR] " + message);
+	}
+
+	public String scanner(String message) {
+		this.println(message);
+		return spaceReplacer(Console.readLine());
+	}
+
+	public String spaceReplacer(String string) {
+		return string.replaceAll(" ", "");
+	}
+}
