d6d79eeb4d40a7384b228c9a0216650c24f94e49
yunsseong <aster20@hanyang.ac.kr>
2024-09-03 14:34:52 +0900

    feat: 리스트 구성이 모두 숫자인지 확인하는 기능 구현

--- src/main/java/lotto/util/Validate.java
+++ src/main/java/lotto/util/Validate.java
@@ -1,10 +1,22 @@
 package lotto.util;
 
+import java.util.List;
+
 public class Validate {
 
 	public static boolean isNumber(String input){
 		try {
 			Integer.parseInt(input);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static boolean isAllNumber(List<String> inputList) {
+		try {
+			inputList.stream()
+				.map(Integer::parseInt);
 			return true;
 		} catch (Exception e) {
 			return false;
