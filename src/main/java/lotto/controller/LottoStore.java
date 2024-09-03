11c3c1a64df38c69ad7552f5d72284b56fe313fa
yunsseong <aster20@hanyang.ac.kr>
2024-09-03 09:46:21 +0900

    feat: 로또 스토어 구매 기능 구현

--- src/main/java/lotto/controller/LottoStore.java
+++ src/main/java/lotto/controller/LottoStore.java
@@ -2,11 +2,9 @@ package lotto.controller;
 
 import java.util.List;
 
-import lotto.model.Lotto;
-
 public interface LottoStore {
 
-	public void buyLotto();
+	public void purchase();
 	public void setHitNumber();
 	public void setHitBonusNumber();
 	public int getLottoAmount();
--- src/main/java/lotto/controller/LottoStoreImpl.java
+++ src/main/java/lotto/controller/LottoStoreImpl.java
@@ -2,10 +2,33 @@ package lotto.controller;
 
 import java.util.List;
 
-public class LottoStoreImpl implements LottoStore{
-	@Override
-	public void buyLotto() {
+import lotto.model.Purchase;
+import lotto.view.Input;
+import lotto.view.InputImpl;
+import lotto.view.Output;
+import lotto.view.OutputImpl;
 
+public class LottoStoreImpl implements LottoStore{
+	private Purchase buyAmount;
+	private final Input input;
+	private final Output output;
+
+	public LottoStoreImpl() {
+		this.input = new InputImpl();
+		this.output = new OutputImpl();
+	}
+
+	@Override
+	public void purchase() {
+		output.printBuyAmountMessage();
+		do {
+			try{
+				this.buyAmount = new Purchase(input.getSingleNumber());
+			} catch (IllegalArgumentException e) {
+				output.printExceptionMessage(e.getMessage());
+			}
+		} while (buyAmount == null);
+		output.printBuyAmount(buyAmount.getPurchaseQuantity());
 	}
 
 	@Override
