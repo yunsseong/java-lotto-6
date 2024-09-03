185cdf00fa22389f4bb7a397fe95c68c14082cdf
yunsseong <aster20@hanyang.ac.kr>
2024-09-03 14:34:38 +0900

    feat: 로또 개수 오류 메세지 추가

--- src/main/java/lotto/view/ExceptionMessage.java
+++ src/main/java/lotto/view/ExceptionMessage.java
@@ -5,4 +5,5 @@ public class ExceptionMessage {
 	public static final String NOT_VALID_PURCHASE_AMOUNT = "구입 금액은 1,000원 이상이여야합니다.";
 	public static final String NOT_IN_LOTTO_NUMBER_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 	public static final String NOT_DIVIDE_INTO_THOUSANDS = "로또 구입 금액은 1000원으로 나누어 떨어져야 합니다.";
+	public static final String INVALID_LOTTO_NUMBER_COUNT = "로또 번호는 숫자 6개로 구성되어야 합니다.";
 }
