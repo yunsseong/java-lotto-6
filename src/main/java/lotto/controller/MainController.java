1c2ab080de8cac9e043353c3c44bf0cfdaabdb6a
yunsseong <aster20@hanyang.ac.kr>
2024-09-03 11:02:05 +0900

    feat: 로또 구매 로직 변경

--- src/main/java/lotto/controller/MainController.java
+++ src/main/java/lotto/controller/MainController.java
@@ -1,16 +1,21 @@
 package lotto.controller;
 
+import lotto.view.Output;
+import lotto.view.OutputImpl;
+
 public class MainController implements Controller{
 	private LottoStore lottoStore;
+	private Output output;
 
 	@Override
 	public void init() {
 		this.lottoStore = new LottoStoreImpl();
+		this.output = new OutputImpl();
 	}
 
 	@Override
 	public void runner() {
-		lottoStore.purchase();
+		output.printLottoList(lottoStore.buyLotto());
 	}
 
 	@Override
