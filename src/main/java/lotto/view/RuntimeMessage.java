fa091049542f7bb97f21f91d3ae203a026d2e267
yunsseong <aster20@hanyang.ac.kr>
2024-09-03 09:42:52 +0900

    feat: 실행 메세지 설정

---
+++ src/main/java/lotto/view/RuntimeMessage.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+public class RuntimeMessage {
+
+	public static final String INPUT_BUY_AMOUNT = "구입금액을 입력해 주세요.";
+	public static final String OUTPUT_BUY_AMOUNT = "개를 구매했습니다.";
+	public static final String INPUT_HIT_NUMBER = "당첨 번호를 입력해 주세요.";
+	public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+	public static final String OUTPUT_HIT_STATICS = "당첨 통계";
+}
