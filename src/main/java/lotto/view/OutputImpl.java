2094ab255fdf9fbaf01fb05d5b395f52e60e1891
yunsseong <aster20@hanyang.ac.kr>
2024-09-03 16:26:04 +0900

    feat: 통합 출력 메서드 구현

--- src/main/java/lotto/view/OutputImpl.java
+++ src/main/java/lotto/view/OutputImpl.java
@@ -6,16 +6,19 @@ public class OutputImpl implements Output{
 
 	@Override
 	public void printExceptionMessage(String message) {
+		System.out.println();
 		System.out.println("[ERROR] " + message);
 	}
 
 	@Override
 	public void printBuyAmountMessage() {
+		System.out.println();
 		System.out.println(RuntimeMessage.INPUT_BUY_AMOUNT);
 	}
 
 	@Override
 	public void printBuyAmount(int buyAmount) {
+		System.out.println();
 		System.out.println(buyAmount + RuntimeMessage.OUTPUT_BUY_AMOUNT);
 	}
 
@@ -27,12 +30,14 @@ public class OutputImpl implements Output{
 
 	@Override
 	public void printInputHitNumberMessage() {
-
+		System.out.println();
+		System.out.println(RuntimeMessage.INPUT_HIT_NUMBER);
 	}
 
 	@Override
 	public void printInputBonusNumber() {
-
+		System.out.println();
+		System.out.println(RuntimeMessage.INPUT_BONUS_NUMBER);
 	}
 
 	@Override
